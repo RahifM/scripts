3cadee11fe92bb4aefe6c2768458514c4c9d1283
RahifM <rahifmanjatha372@gmail.com>
2025-05-20 02:53:28 +0530

    tgbot

---
+++ bot.sh
@@ -0,0 +1,4 @@
+export botmsg="curl -s -X POST "https://api.telegram.org/bot${botapi}/sendMessage" -d chat_id="${chatid}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text"
+
+$botmsg="WHATSUP"
+$botmsg="WHATSUPWHATSUP"
