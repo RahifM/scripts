47d54dc25b1da1eace464c1e1a77d1f579a4309e
RahifM <rahifmanjatha372@gmail.com>
2025-07-27 10:54:38 +0530

    new gitpod script

---
+++ ffmpeg_encode_gp2/f-new2.sh
@@ -0,0 +1,59 @@
+export botmsg="curl -s -X POST "https://api.telegram.org/bot${BOTAPI}/sendMessage" -d chat_id="${CHATID}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text"
+
+cd tm && time ./tm2.sh || mkdir tm && cd tm && wget https://raw.githubusercontent.com/RahifM/scripts/refs/heads/newffmpeg/tm2.sh && chmod +x tm2.sh && time ./tm2.sh
+
+transmission-remote --start-paused -a "https://github.com/zmzu/dump/releases/download/1.0/Bleach.Box.1-6.1080p.BluRay.HEVC.AAC2.0.x265-RB26DETT.torrent" &&
+	transmission-remote -t 1 -G all && transmission-remote -t 1 -g5 && transmission-remote -t 1 -f | grep Yes && transmission-remote -t 1 -s
+
+export i=Bleach.E006.1080p.BluRay.HEVC.AAC2.0.x265-RB26DETT.mkv
+export o=Bleach.E006.mkv
+
+fmpg() {
+
+	cd Bl* && cd Bl* && pwd && du -hs *
+
+	time sudo apt install ffmpeg -y
+
+echo -e "\nPlease set input and input! (only 720p encodes supported as of now)\n"
+echo ${i}
+echo ${o}
+#read -e -p "Input: " i
+#read -e -p "Output: " o
+
+
+if [ -z "$i" -o -z "$o" ]; then
+       echo -e "\nNo input or output found\n"                        exit 1
+fi                                                                                                                          $botmsg="${o} encode started"
+
+echo -e "\n"
+
+low() {
+        rm ${o}*.txt
+        time ffmpeg -i  "${i}" -s 1280x720 -c copy -map 0 -c:v libx265 -x265-params crf=28 -pix_fmt yuv420p -preset slow  "${o}" 2>&1 | tee ${o}-$(date +'%Y%m%d-%H%M').txt
+}
+
+up() {
+[ -f "github-release-2.0.0.2-ubuntu" ] && echo "gh rel already exists" || ( echo "gh rel not found" && time wget https://github.com/tfausak/github-release/releases/download/2.0.0.2/github-release-2.0.0.2-ubuntu && chmod +x github-release-2.0.0.2-ubuntu )
+
+./gi* upload --token $GHSECRET --owner 'zmzu' --repo 'be_dump' --tag '1.0' --file ${o} --name ${o}
+}
+
+low
+
+[ -f ${o} ] && up && echo "${o} encode / upload success" && $botmsg="${o} encode / upload success" || ( echo "${o} encode / upload failed" && $botmsg="${o} encode / upload failed" )
+}
+
+while true ; do
+	if [ "$(transmission-remote -t 1 -i | grep State)" = "  State: Idle" ] ; then
+		echo "Idling, starting encode"
+		transmission-remote -t 1 -S
+		ls && pwd
+		fmpg
+		pwd && du -hs *
+		break
+	fi
+	echo $(transmission-remote -t 1 -i | grep State)
+	echo $(transmission-remote -t 1 -i | grep Percent)
+	echo "check back after 15s"
+  sleep 15
+done
