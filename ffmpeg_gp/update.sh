43f6cc422a4a3d91b34f00db471a4c0459c83873
RahifM <rahifmanjatha372@gmail.com>
2025-08-27 09:40:00 +0530

    update: specifiy the newffmpeg branch

--- ffmpeg_gp/update.sh
+++ ffmpeg_gp/update.sh
@@ -1,3 +1,3 @@
-[ -f $HOME/scripts ] && rm -rf $HOME/scripts && git clone https://github.com/RahifM/scripts $HOME/scripts
+[ -f $HOME/scripts ] && rm -rf $HOME/scripts && git clone https://github.com/RahifM/scripts -b newffmpeg $HOME/scripts
 
 cp $HOME/scripts/ffmpeg_gp/ffmpeg.sh $(pwd)/
