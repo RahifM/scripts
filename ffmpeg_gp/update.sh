e060d0bcc68ca259b48298ecb2a902fad25608b0
RahifM <rahifmanjatha372@gmail.com>
2025-08-27 11:54:58 +0530

    update: fix no clone if file did not exist

--- ffmpeg_gp/update.sh
+++ ffmpeg_gp/update.sh
@@ -1,3 +1,5 @@
-[ -d $HOME/scripts ] && rm -rf $HOME/scripts && git clone https://github.com/RahifM/scripts -b newffmpeg $HOME/scripts
+[ -d $HOME/scripts ] && rm -rf $HOME/scripts
+
+git clone https://github.com/RahifM/scripts -b newffmpeg $HOME/scripts
 
 cp $HOME/scripts/ffmpeg_gp/ffmpeg.sh $(pwd)/
