9c2b15f35a78dea88408aea97a4c110af894d2ae
RahifM <rahifmanjatha372@gmail.com>
2025-08-29 19:43:44 +0530

    include update script into setup

--- ffmpeg_gp/setup.sh
+++ ffmpeg_gp/setup.sh
@@ -1,6 +1,12 @@
 sudo apt update
 sudo apt install screen ffmpeg -y
 
-time bash $HOME/scripts/misc.sh || { git clone https://github.com/RahifM/scripts $HOME/scripts && bash $HOME/scripts/misc.sh ; }
+[ -d $HOME/scripts ] && rm -rf $HOME/scripts
+
+git clone https://github.com/RahifM/scripts -b newffmpeg $HOME/scripts
+
+bash $HOME/scripts/misc.sh
+
+cp $HOME/scripts/ffmpeg_gp/ffmpeg.sh $(pwd)/
 
 screen -S 1 && screen -x 1
--- ffmpeg_gp/update.sh
+++
@@ -1,5 +0,0 @@
-[ -d $HOME/scripts ] && rm -rf $HOME/scripts
-
-git clone https://github.com/RahifM/scripts -b newffmpeg $HOME/scripts
-
-cp $HOME/scripts/ffmpeg_gp/ffmpeg.sh $(pwd)/
