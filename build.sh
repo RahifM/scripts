efd4abc9ddfb3febb179fc9e7cf28fb80ac087f2
Rahif M <rahifmanjatha372@gmail.com>
2025-11-03 01:37:25 +0530

    non rv los 18.1

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 crave run  --no-patch -- "rm -rf .repo/local_manifests; \
 sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5; \
 sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6 /usr/lib/x86_64-linux-gnu/libtinfo.so.5; \
-git clone https://github.com/RahifM/local_manifests --depth 1 -b lineage-18.1-rv-beryllium .repo/local_manifests; \
+repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs; \
+git clone https://github.com/RahifM/local_manifests --depth 1 -b lineage-18.1-beryllium .repo/local_manifests; \
 /opt/crave/resync.sh; \
 export TZ=Asia/Kolkata && echo $(date); \
 source build/envsetup.sh; \
