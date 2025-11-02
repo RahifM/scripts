8168e7f9c2a75cfe021afe7c9993ae549fab3dea
Rahif M <rahifmanjatha372@gmail.com>
2025-11-02 11:13:43 +0530

    echo date

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/l
 sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6 /usr/lib/x86_64-linux-gnu/libtinfo.so.5; \
 git clone https://github.com/RahifM/local_manifests --depth 1 -b lineage-18.1-rv-beryllium .repo/local_manifests; \
 /opt/crave/resync.sh; \
-export TZ=Asia/Kolkata; \
+export TZ=Asia/Kolkata && echo $(date); \
 source build/envsetup.sh; \
 lunch lineage_beryllium-user; \
 m bacon"
