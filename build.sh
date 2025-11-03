a62d6fa9d567554a6374ce3237efb816e7251132
Rahif M <rahifmanjatha372@gmail.com>
2025-11-03 14:37:59 +0530

    add installclean and start asb pick

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-crave run  --no-patch -- "rm -rf .repo/local_manifests; \
+run  --no-patch -- "rm -rf .repo/local_manifests; \
 sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5; \
 sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6 /usr/lib/x86_64-linux-gnu/libtinfo.so.5; \
 repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs; \
@@ -6,5 +6,7 @@ git clone https://github.com/RahifM/local_manifests --depth 1 -b lineage-18.1-be
 /opt/crave/resync.sh; \
 export TZ=Asia/Kolkata && echo $(date); \
 source build/envsetup.sh; \
+repopick -t R_asb_2024-03; \
 lunch lineage_beryllium-user; \
+make installclean; \
 m bacon"
