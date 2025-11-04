d28af0e50bb9c7ed03bf4b88c979082f84ab01bf
Rahif M <rahifmanjatha372@gmail.com>
2025-11-04 22:21:43 +0530

    asb patch 2024-05 seems to have pick issue

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+set -e
+
 crave run  --no-patch -- "rm -rf .repo/local_manifests; \
 sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5; \
 sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6 /usr/lib/x86_64-linux-gnu/libtinfo.so.5; \
@@ -6,7 +8,8 @@ git clone https://github.com/RahifM/local_manifests --depth 1 -b lineage-18.1-be
 /opt/crave/resync.sh; \
 export TZ=Asia/Kolkata && echo $(date); \
 source build/envsetup.sh; \
-repopick -t R_asb_2024-03 && repopick -t R_asb_2024-04 && repopick -t R_asb_2024-05 && repopick -t R_asb_2024-06 && repopick -t R_asb_2024-07 && repopick -t R_asb_2024-08 && repopick -t R_asb_2024-09 && repopick -t R_asb_2024-10 && repopick -t R_asb_2024-11 && repopick -t R_asb_2024-12 && repopick -t R_asb_2025-01 && repopick -t R_asb_2025-02 && repopick -t R_asb_2025-03 && repopick -t R_asb_2025-04 && repopick -t R_asb_2025-05 && repopick -t R_asb_2025-06 && repopick -t R_asb_2025-09; \
+repopick -t R_asb_2024-03 && repopick -t R_asb_2024-04 && repopick -t R_asb_2024-05; \
+#&& repopick -t R_asb_2024-06 && repopick -t R_asb_2024-07 && repopick -t R_asb_2024-08 && repopick -t R_asb_2024-09 && repopick -t R_asb_2024-10 && repopick -t R_asb_2024-11 && repopick -t R_asb_2024-12 && repopick -t R_asb_2025-01 && repopick -t R_asb_2025-02 && repopick -t R_asb_2025-03 && repopick -t R_asb_2025-04 && repopick -t R_asb_2025-05 && repopick -t R_asb_2025-06 && repopick -t R_asb_2025-09; \
 lunch lineage_beryllium-user; \
 make installclean; \
 m bacon"
