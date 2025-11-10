ae1c0873f54d46fb7e4b07238d29cc02344ff2ca
RahifM <rahifmanjatha372@gmail.com>
2025-11-10 23:37:01 +0530

    Revert "linkers no longer needed?"
    This reverts commit c551310f2072adb8eaf0fb21e986a5421b3f8f42.

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -e
 
 rm -rf .repo/local_manifests
-#sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5
-#sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6 /usr/lib/x86_64-linux-gnu/libtinfo.so.5
+sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5
+sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6 /usr/lib/x86_64-linux-gnu/libtinfo.so.5
 repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs
 #git clone https://github.com/RahifM/local_manifests --depth 1 -b lineage-18.1-beryllium .repo/local_manifests
 /opt/crave/resync.sh
