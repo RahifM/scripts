64e4292b49dc4559dcf2507950ba556015b4b9f1
RahifM <rahifmanjatha372@gmail.com>
2025-11-10 23:38:32 +0530

    crave run is the only way it seems

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
-set -e
-
-crave run  --no-patch -- "rm -rf .repo/local_manifests; \
+crave run  --no-patch -- "set -e; \
+rm -rf .repo/local_manifests; \
 sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5; \
 sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6 /usr/lib/x86_64-linux-gnu/libtinfo.so.5; \
 repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs; \
