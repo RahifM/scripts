cd0c764f9fe66157440a42b74b2ea97bb3500aa5
RahifM <rahifmanjatha372@gmail.com>
2025-11-11 21:42:09 +0530

    try without crave run once more

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
-crave run  --no-patch -- "set -e; \
-rm -rf .repo/local_manifests; \
-repo init -u https://github.com/LineageOS/android.git -b cm-14.1 --git-lfs; \
-git clone https://github.com/RahifM/local_manifests --depth 1 -b cm-14.1 .repo/local_manifests; \
-/opt/crave/resync.sh; \
-export TZ=Asia/Kolkata && echo $(date); \
-export LC_ALL=C; \
-sudo sed -i 's|jdk.tls.disabledAlgorithms=SSLv3, TLSv1, TLSv1.1|jdk.tls.disabledAlgorithms=SSLv3|g' /etc/java-8-openjdk/security/java.security; \
-source build/envsetup.sh; \
-lunch lineage_wt88047-user; \
-make installclean; \
-m bacon"
+set -e
+
+rm -rf .repo/local_manifests
+repo init -u https://github.com/LineageOS/android.git -b cm-14.1 --git-lfs
+git clone https://github.com/RahifM/local_manifests --depth 1 -b cm-14.1 .repo/local_manifests
+/opt/crave/resync.sh
+export TZ=Asia/Kolkata && echo $(date)
+export LC_ALL=C
+sudo sed -i 's|jdk.tls.disabledAlgorithms=SSLv3, TLSv1, TLSv1.1|jdk.tls.disabledAlgorithms=SSLv3|g' /etc/java-8-openjdk/security/java.security
+source build/envsetup.sh
+lunch lineage_wt88047-user
+make installclean
+m bacon
