33c3b0e60a8201a86b7c2923b5af604f45475de7
RahifM <rahifmanjatha372@gmail.com>
2025-11-11 10:41:08 +0530

    init cm-14.1 wt88047

--- build.sh
+++ build.sh
@@ -1,16 +1,10 @@
 crave run  --no-patch -- "set -e; \
 rm -rf .repo/local_manifests; \
-sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5; \
-sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6 /usr/lib/x86_64-linux-gnu/libtinfo.so.5; \
-repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs; \
+repo init -u https://github.com/LineageOS/android.git -b cm-14.1 --git-lfs; \
+git clone https://github.com/RahifM/local_manifests --depth 1 -b cm-14.1 .repo/local_manifests; \
 /opt/crave/resync.sh; \
 export TZ=Asia/Kolkata && echo $(date); \
 source build/envsetup.sh; \
-repopick -t R_asb_2024-03 && repopick -t R_asb_2024-04; \
-rm -rf external/sonivox && echo "rm done"; \
-git clone https://github.com/LineageOS/android_external_sonivox -b lineage-18.1 external/sonivox && echo "clone done"; \
-repopick -t R_asb_2024-05; \
-repopick -t R_asb_2024-06 && repopick -t R_asb_2024-07 && repopick -t R_asb_2024-08 && repopick -t R_asb_2024-09 && repopick -t R_asb_2024-10 && repopick -t R_asb_2024-11 && repopick -t R_asb_2024-12 && repopick -t R_asb_2025-01 && repopick -t R_asb_2025-02 && repopick -t R_asb_2025-03 && repopick -t R_asb_2025-04 && repopick -t R_asb_2025-05 && repopick -t R_asb_2025-06 && repopick -t R_asb_2025-09; \
-lunch lineage_beryllium-user; \
+lunch lineage_wt88047-user; \
 make installclean; \
 m bacon"
