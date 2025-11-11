47e2b6da0bf4ed4be67169b23c6c1f02a6beda5e
RahifM <rahifmanjatha372@gmail.com>
2025-11-11 12:11:43 +0530

    fix build

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ repo init -u https://github.com/LineageOS/android.git -b cm-14.1 --git-lfs; \
 git clone https://github.com/RahifM/local_manifests --depth 1 -b cm-14.1 .repo/local_manifests; \
 /opt/crave/resync.sh; \
 export TZ=Asia/Kolkata && echo $(date); \
+export LC_ALL=C; \
 source build/envsetup.sh; \
 lunch lineage_wt88047-user; \
 make installclean; \
