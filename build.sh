3d8f47c88bf825cd8370721c4bef751dc889c955
RahifM <rahifmanjatha372@gmail.com>
2025-12-14 14:32:12 +0530

    asb 2025-11

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ export TZ=Asia/Kolkata && echo $(date)
 
 source build/envsetup.sh
 
-repopick -t R_asb_2024-03 && repopick -t R_asb_2024-04 && repopick -t R_asb_2024-05 && repopick -t R_asb_2024-06 && repopick -t R_asb_2024-07 && repopick -t R_asb_2024-08 && repopick -t R_asb_2024-09 && repopick -t R_asb_2024-10 && repopick -t R_asb_2024-11 && repopick -t R_asb_2024-12 && repopick -t R_asb_2025-01 && repopick -t R_asb_2025-02 && repopick -t R_asb_2025-03 && repopick -t R_asb_2025-04 && repopick -t R_asb_2025-05 && repopick -t R_asb_2025-06 && repopick -t R_asb_2025-09
+repopick -t R_asb_2024-03 && repopick -t R_asb_2024-04 && repopick -t R_asb_2024-05 && repopick -t R_asb_2024-06 && repopick -t R_asb_2024-07 && repopick -t R_asb_2024-08 && repopick -t R_asb_2024-09 && repopick -t R_asb_2024-10 && repopick -t R_asb_2024-11 && repopick -t R_asb_2024-12 && repopick -t R_asb_2025-01 && repopick -t R_asb_2025-02 && repopick -t R_asb_2025-03 && repopick -t R_asb_2025-04 && repopick -t R_asb_2025-05 && repopick -t R_asb_2025-06 && repopick -t R_asb_2025-09 && repopick -t R_asb_2025-11
 
 lunch lineage_beryllium-user
 make installclean
