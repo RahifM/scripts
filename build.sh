64dd852bed146f96627e972f904384c767fe6c38
RahifM <rahifmanjatha372@gmail.com>
2026-06-18 03:25:58 +0530

    Revert "try the fuck jack script"
    This reverts commit 12be1be8a6127c4244f12bc926f352e06c21e67d.

--- build.sh
+++ build.sh
@@ -33,12 +33,8 @@ export LC_ALL=C
 java -version
 javac -version
 pwd && ls
-export USE_NINJA=false
-rm -rf ~/.jack*
-export ANDROID_JACK_VM_ARGS="-Xmx4g -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
-./prebuilts/sdk/tools/jack-admin kill-server
-./prebuilts/sdk/tools/jack-admin start-server
 source build/envsetup.sh
 lunch lineage_wt88047-user
 make installclean
 m bacon
+ls ~/.jack-server
